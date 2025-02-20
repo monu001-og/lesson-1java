cdeec38c1a849467101322a160e014227ef972dc
monu001-og <olatudeogbe@gmail.com>
2025-02-20 07:13:35 +0100

    history.java

---
+++ history.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class history{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Hello User please enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Enter your lucky number");
+        int lucky=sc.nextInt();
+        System.out.println("Hello "+name +", Your lucky number" +lucky +" shows you are really hardworking.Keep it up!");
+
+
+    }
+}
